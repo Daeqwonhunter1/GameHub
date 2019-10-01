7186d26d38a35624223dfd366e3c8774eb682b50
DHunter <dhunter@DHunters-MacBook-Air.local>
2019-10-01 15:13:39 -0400

    Issue with Appending API Data to screen

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const button = document.querySelector('button')
+const input = document.getElementById('inp')
+const display = document.querySelector('api-div')
 
-button.addEventListener('click', async () => {
-  axios.get({
-      url: "https://api-v3.igdb.com/games",
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'user-key': 08 c38743d7290d45bc9d8775857416a7
-      },
-      data: "fields age_ratings,aggregated_rating,aggregated_rating_count,alternative_names,artworks,bundles,category,collection,cover,created_at,dlcs,expansions,external_games,first_release_date,follows,franchise,franchises,game_engines,game_modes,genres,hypes,involved_companies,keywords,multiplayer_modes,name,parent_game,platforms,player_perspectives,popularity,pulse_count,rating,rating_count,release_dates,screenshots,similar_games,slug,standalone_expansions,status,storyline,summary,tags,themes,time_to_beat,total_rating,total_rating_count,updated_at,url,version_parent,version_title,videos,websites;"
-    })
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(err => {
-      console.error(err);
-    });
+
+const displayGames = (array) => {
+  displayGames.innerHTML = ''
+  array.forEach((game) => {
+    const gameDiv = document.createElement("div");
+    gameDiv.innerHTML = `<h1>${game.name}</h1> `
+  })
+  display.append(gameDiv);
+}
+
+
+
+
+button.addEventListener("click", async () => {
+  const response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api-v3.igdb.com/games?search=${input.value}&fields=id,name,cover`, {
+
+    headers: {
+      "user-key": '08c38743d7290d45bc9d8775857416a7'
+    }
+  })
+
+
+  displayGames(response.data)
+  console.log(response.data)
 })
